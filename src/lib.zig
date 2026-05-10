f2c5d0c9741313857ab70adc6224053ec160f22a
Nathaniel John Morton <nathan.j.morton@gmail.com>
2026-05-10 14:57:12 -0400

    release: v0.4.0

--- src/lib.zig
+++ src/lib.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 // ── Version ───────────────────────────────────────────────────────────────────
 
-pub const VERSION = "0.3.0";
+pub const VERSION = "0.4.0";
 
 // ── Templates ────────────────────────────────────────────────────────────────
 // Placeholders replaced at runtime (not fmt strings — safe to contain { }):
